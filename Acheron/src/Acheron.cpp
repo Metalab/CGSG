e69d77d3e90aa26d4ea9f0601cf9d7172c9cbf02
max <max>
2009-04-20 20:14:36 +0000

    git-svn-id: file:///tmp/cgsg@34 7d7550ab-71c3-48bc-b1bb-aab3a564f44c

---
+++ Acheron/src/Acheron.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Acheron.cpp
+// Author      : max
+// Version     :
+// Copyright   : copyleft ftw
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+
+using namespace std;
+
+int main( int argc, char ** argv ) {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}

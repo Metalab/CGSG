88b3ceecd9fae8542df9e7734da615dac66cd2e0
kintel <kintel>
2010-07-17 20:29:04 +0000

    Extract uniforms and attributes from shaders
    git-svn-id: file:///tmp/cgsg@109 7d7550ab-71c3-48bc-b1bb-aab3a564f44c

---
+++ webgl/glpad/shaderutils.js
@@ -0,0 +1,19 @@
+var ExtractUniformsFromShaderSource = function(source) {
+    var reg = new RegExp("\uniform ((bool|int|uint|float|[biu]?vec[234]|mat[234]x?[234]?) ([A-Za-z0-9]*));", "gi");
+    var tmp;
+    var uniforms = [];
+    while (tmp = reg.exec(source)) {
+	uniforms.push(tmp[3]);
+    }
+    return uniforms;
+}
+
+var ExtractAttributesFromShaderSource = function(source) {
+    var reg = new RegExp("\attribute ((bool|int|uint|float|[biu]?vec[234]|mat[234]x?[234]?) ([A-Za-z0-9]*));", "gi");
+    var tmp;
+    var attributes = [];
+    while (tmp = reg.exec(source)) {
+	attributes.push(tmp[3]);
+    }
+    return attributes;
+}

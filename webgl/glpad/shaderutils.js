c4bb1bd0d7663f95cbcb495e663de89eec9d6283
kintel <kintel>
2010-07-18 18:58:27 +0000

    bugfix: Port to WebKit
    git-svn-id: file:///tmp/cgsg@135 7d7550ab-71c3-48bc-b1bb-aab3a564f44c

--- webgl/glpad/shaderutils.js
+++ webgl/glpad/shaderutils.js
@@ -1,5 +1,8 @@
-var ExtractUniformsFromShaderSource = function(source) {
-    var reg = new RegExp("\uniform ((bool|int|uint|float|[biu]?vec[234]|mat[234]x?[234]?|sampler2D) ([A-Za-z0-9]*));", "gi");
+function ExtractUniformsFromShaderSource(source) {
+    var reg = 
+        new RegExp(
+            "uniform ((bool|int|uint|float|[biu]?vec[234]|mat[234]x?[234]?|sampler2D) ([A-Za-z0-9]*));",
+            "gi");
     var tmp;
     var uniforms = [];
     while (tmp = reg.exec(source)) {
@@ -8,8 +11,8 @@ var ExtractUniformsFromShaderSource = function(source) {
     return uniforms;
 }
 
-var ExtractAttributesFromShaderSource = function(source) {
-    var reg = new RegExp("\attribute ((bool|int|uint|float|[biu]?vec[234]|mat[234]x?[234]?) ([A-Za-z0-9]*));", "gi");
+function ExtractAttributesFromShaderSource(source) {
+    var reg = new RegExp("attribute ((bool|int|uint|float|[biu]?vec[234]|mat[234]x?[234]?) ([A-Za-z0-9]*));", "gi");
     var tmp;
     var attributes = [];
     while (tmp = reg.exec(source)) {
